b0715754d83611ce5b104a6b19ac2a543af63233
victinzhong <victinzhong@163.com>
2022-10-02 13:20:55 +0800

    样式修改

--- src/pages/home/menus/index.js
+++ src/pages/home/menus/index.js
@@ -7,9 +7,15 @@ function Menu() {
     const dispatch = useDispatch()
     return (
         <div className="Menu">
-            左侧菜单栏 <br />
-            <button onClick={() => dispatch(increment('card'))}>addCard</button>
-            <div style={{height:'10000px'}}>1233</div>
+            <h2>基础组件</h2>
+            <div className='Menu-list'>
+                <button onClick={() => dispatch(increment('card'))}>卡片</button>
+                <button onClick={() => dispatch(increment('button'))}>按钮</button>
+            </div>
+            <h2>基础组件</h2>
+            <div style={{height:'10000px'}}>
+                more
+            </div>
         </div>
     );
 }
