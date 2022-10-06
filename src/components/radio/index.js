b10090dcada07f6280af91f21e835f4df1672bcc
CXG <ethanc123@foxmail.com>
2022-10-06 19:05:10 +0800

    单选框修改中

--- src/components/radio/index.js
+++ src/components/radio/index.js
@@ -1,22 +1,32 @@
 import { Radio } from 'antd';
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import {changeId} from '../../pages/home/slice'
+import { changeId } from '../../pages/home/slice'
 
 function App(options) {
   const dispatch = useDispatch()
   const { autoFocus, defaultChecked, disabled, text } = options.options;
-  console.log(options.options);
+
+  const content = text;
   return (
-    <Radio 
-      autoFocus={autoFocus || false} 
-      defaultChecked={defaultChecked || false} 
-      disabled={disabled || false} 
-      onClick={() => { dispatch(changeId(options.options))}}>
-        {text}
-    </Radio>
+    // <Radio.Group
+    //   options={content}
+    //   autoFocus={autoFocus || false}
+    //   defaultChecked={defaultChecked || false}
+    //   disabled={disabled || false}
+    //   onClick={() => { dispatch(changeId(options.options)) }}
+    // />
+    <Radio.Group>
+      <Radio ></Radio>
+      <Radio ></Radio>
+      <Radio></Radio>
+      <Radio></Radio>
+      <Radio></Radio>
+    </Radio.Group>
+
+
   )
 }
-    
+
 
 export default App;
