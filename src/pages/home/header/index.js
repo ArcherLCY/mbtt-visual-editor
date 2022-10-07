4095c1a64355ba0f4fe8dc23cd3af5dddea5e3e8
victinzhong <victinzhong@163.com>
2022-10-07 10:47:33 +0800

    解决冲突

--- src/pages/home/header/index.js
+++ src/pages/home/header/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Card, Col, Row, Image, Button, Popover } from 'antd';
 import { LeftOutlined } from '@ant-design/icons';
+import Display from './compontents/display'
+import ContentSizeEdit from './compontents/ContentSizeEdit'
+import IsEdit from './compontents/IsEdit'
 
-import './headerStyle.css';
+import styles from './index.module.scss';
 
 function Header() {
   // 定义模板市场浮窗内容
@@ -53,16 +56,26 @@ function Header() {
   );
 
   return (
-    <div className="Header">
-      <Card hoverable bordered='true'>
-        <LeftOutlined />
-        未命名应用
-        表单设计
-        <Popover content={modelMarket}>
+    <Card hoverable className={styles.header_card}>     
+        <div className={styles.header}>
+          <div>
+            <LeftOutlined />
+            <span style={{marginLeft: 10 + 'px'}}>未命名应用 自由表单设计</span>
+          </div>
+          <div className={styles.header_right}>
+          <Popover content={modelMarket}>
           <Button>模板市场</Button>
         </Popover>
+
+            {/* 辑和预览切换的组件 */}
+            <IsEdit/>  
+            {/* 操作按钮 */}
+            <Display />
+            {/* 配置面板 */}
+            <ContentSizeEdit/>
+          </div>
+        </div>
       </Card>
-    </div>
   );
 }
 
